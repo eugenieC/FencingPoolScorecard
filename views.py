f7af335eb9bd73b37b66d78f6be9d4448af34324
Eugenie Cha <40417846+eugenieC@users.noreply.github.com>
2023-02-14 22:11:32 -0800

    Update views.py

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 options = webdriver.ChromeOptions()
 options.add_argument("--no-sandbox")
